ce18a024df39e3c7a26fbe3f04ba90e16fe3f0e2
eduardosmatheus <matheus-edu@outlook.com>
2017-05-26 13:06:20 -0300

    Ajusta URL.

--- routes/comments.js
+++ routes/comments.js
@@ -6,14 +6,14 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 const Comment = require('../model/comment');
 
-router.get('places/:placeId/comments', (req, res, next) => {
+router.get('/places/:placeId/comments', (req, res, next) => {
   Comment.find({ placeId: req.params.placeId }, (err, comments) => {
     if(err) throw err;
     res.send(comments);
   });
 });
 
-router.post('places/:placeId/comments', (req, res, next) => {
+router.post('/places/:placeId/comments', (req, res, next) => {
   const comment = new Comment({
     _id: new ObjectId(),
     placeId: req.body.placeId,
