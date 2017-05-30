2d36270ad7b6909d8dc9ce8cffd8d95232b734e6
eduardosmatheus <matheus-edu@outlook.com>
2017-05-30 13:18:40 -0300

    Retorna todos os comentários ao criar um novo.

--- routes/comments.js
+++ routes/comments.js
@@ -17,14 +17,15 @@ router.get('/:placeId/comments', (req, res) => {
 });
 
 router.post('/:placeId/comments', (req, res) => {
-  const comment = new Comment({
-    _id: new ObjectId(),
-    placeId: req.body.placeId,
-    content: req.body.content
-  });
-
-  comment.save().then(result => {
-    res.send(result);
+  new Comment({ _id: new ObjectId(), placeId: req.body.placeId, content: req.body.content })
+  .save().then(result => {
+    if (result) {
+      Comment.find({ placeId: req.params.placeId }, (err, comments) => {
+        res.send(comments);
+      });
+    } else {
+      res.status(400).send("Ocorreram problemas ao salvar o comentário.");
+    }
    });
 });
 
